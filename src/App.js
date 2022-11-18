ed72f4ff8194a1aa69661e6f84aca0fcb75c766b
carlosHidalgo95 <karucupcake@gmail.com>
2022-11-18 21:20:33 +0100

    connected to test api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  let [pageNumber,setPageNumber]=useState(1);
+  let [fetchedData,updateFetchData] = useState([]);
+
+  //Info y results son parte del JSON que devuelve la api de rick y morty
+  let {info,results}=fetchedData;
+
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+  console.log(results);
+
+  useEffect(()=>{
+    //IIFE
+    (async function(){
+      let data= await fetch(api).then(res=>res.json())
+      updateFetchData(data);
+    })()
+
+  },[api])
+  
   return (
     <div className="App">
       
