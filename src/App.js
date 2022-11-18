aebc7c243889ce76084e9bdc48b90b9950a94c60
AdryDevs <alonsoadrian86@gmail.com>
2022-11-18 21:21:22 +0100

    home structure done and imported from App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 
+// Import the component
+
+import Home from './Containers/Home/Home';
+
 function App() {
   return (
     <div className="App">
       
+      <Home />
+
     </div>
   );
 }
