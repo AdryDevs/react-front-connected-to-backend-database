9e06d46a25ca91306c22c2f082218d92a2e24515
AdryDevs <alonsoadrian86@gmail.com>
2022-11-19 00:51:45 +0100

    styles home

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { Carousel } from 'antd';
+import './Home.scss'
 
-const Home = () => (
-    <div>
-                
-    </div>
-)
+const contentStyle = {
+    margin: 0,
+    height: '90vh',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#000000',
+  };
+
+const Home = () => {
+    const onChange = (currentSlide) => {
+        console.log(currentSlide);
+      };
+      return (
+
+        <Carousel afterChange={onChange}>
+          <div className='homeDesign'>
+            <h3 style={contentStyle}>Picture1</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>Picture2</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>Picture3</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>Picture4</h3>
+          </div>
+        </Carousel>
+
+      );
+    };
 
 export default Home
