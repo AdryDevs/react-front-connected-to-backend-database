d02eab3318cdd025c8db3cd6ac3004c6518f7de6
AdryDevs <alonsoadrian86@gmail.com>
2022-11-19 20:01:44 +0100

    movie details almost done

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -6,7 +6,8 @@ import './Home.scss'
 const Home = () => {
       return (
         <div className="homeDesign">
-          <img alt='logo' className='logo' src='' /> //TODO: add logo
+          <img alt='logo' className='logo' src='' /> 
+          {/* TODO: add logo */}
           <img alt='corn' src='https://images.pexels.com/photos/33129/popcorn-movie-party-entertainment.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' />
         </div>
       );
