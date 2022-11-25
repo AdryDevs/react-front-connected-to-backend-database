4626698d01b67d72db55c8524988420f749b6515
Juan Genovard <juangenovardcaballero@gmail.com>
2022-11-25 20:14:00 +0100

    Carlos me mete prisa

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,16 +7,16 @@ function Header() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Home</Navbar.Brand>
+        <Navbar.Brand href="../../Containers/Home">Home</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Login</Nav.Link>
-            <Nav.Link href="#link">Register</Nav.Link>
-            <Nav.Link href="#link">Movies</Nav.Link>
+            <Nav.Link href="../../Containers/Login">Login</Nav.Link>
+            <Nav.Link href="../../Containers/Register">Register</Nav.Link>
+            <Nav.Link href="../../Containers/Movies">Movies</Nav.Link>
             <Nav.Link href="#link">Series</Nav.Link>
             <NavDropdown title="User" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Settings</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
               </NavDropdown.Item>
