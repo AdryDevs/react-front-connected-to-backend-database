6b1ef361940b548f64ef40e3959997a491b49e97
AdryDevs <alonsoadrian86@gmail.com>
2022-11-25 18:28:24 +0100

    ant design deleted, starting with boostrap

--- src/Containers/Movies/Movies.jsx
+++ src/Containers/Movies/Movies.jsx
@@ -1,42 +1,15 @@
 import React from 'react'
-import { Card } from 'antd'
 import "./Movies.scss";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-const { Meta } = Card
 
 const Movies = () => {
   return (
     <div className='moviesDesign'>
-      <Card
-        hoverable
-        style={{ width: 230 }}
-        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-      >
-        <Meta title="Europe Street beat" description="www.instagram.com" />
-      </Card>
-
-      <Card
-        hoverable
-        style={{ width: 230 }}
-        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-      >
-        <Meta title="Europe Street beat" description="www.instagram.com" />
-      </Card>
-
-      <Card
-        hoverable
-        style={{ width: 230 }}
-        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-      >
-        <Meta title="Europe Street beat" description="www.instagram.com" />
-      </Card>
+      
     </div>
   )
 }
-
-
-
 
 
 export default Movies
