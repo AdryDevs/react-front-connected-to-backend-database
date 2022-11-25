8942c55e014498d62141729a13efc90907627b8e
Juan Genovard <juangenovardcaballero@gmail.com>
2022-11-25 19:46:41 +0100

    Nuevo Header con bootstrap

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,52 +1,21 @@
 import React from 'react';
-import { Menu, Row, Col } from 'antd';
-import { SettingOutlined, UserOutlined, LogoutOutlined } from '@ant-design/icons';
-import { Navigate,useNavigate } from 'react-router-dom';
-const Header = () => {
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 
-    const navigate=useNavigate();
-    return(
-        <Row>
-  <Menu mode="horizontal" defaultSelectedKeys={['mail']}>
-  <Col xs={3} sm={3} md={3} lg={3}>
-    <Menu.Item >
-    <Menu.Item className="headerWord" onClick={() => navigate("/login")}>Login</Menu.Item>
-    </Menu.Item>
-    </Col>
-    <Col xs={3} sm={3} md={3} lg={3}>
-    <Menu.Item >
-    <Menu.Item className="headerWord" onClick={() => navigate("/register")}>Register</Menu.Item>
-    </Menu.Item>
-    </Col>
-    <Col xs={3} sm={3} md={3} lg={3}>
-    <Menu.Item >
-    <Menu.Item className="headerWord" onClick={() => navigate("/movies")}>Movies</Menu.Item>
-    </Menu.Item>
-    </Col>
-    <Col xs={3} sm={3} md={3} lg={3}>
-    <Menu.Item >
-    <Menu.Item className="headerWord" onClick={() => navigate("/series")}>Series</Menu.Item>
-    </Menu.Item>
-    </Col>
-    {/* <Col xs={3} sm={3} md={3} lg={3}>
-    <Menu.Item >
-    <Menu.Item className="headerWord" onClick={() => navigate("/login")}>Login</Menu.Item>
-    </Menu.Item>
-    </Col> */}
-    <Col xs={3} sm={3} md={3} lg={3}>
-        
-    <Menu.SubMenu key="SubMenu" title="User" icon={<UserOutlined />}>
-      <Menu.Item key="two" icon={<SettingOutlined />}>
-        Modify Profile
-      </Menu.Item>
-      <Menu.Item key="two" icon={<LogoutOutlined />}>
-        Logout
-      </Menu.Item>
-      
-    </Menu.SubMenu>
-    </Col>
-  </Menu>
-  </Row>
-    )
-    };
-export default Header;
+function TextLinkExample() {
+  return (
+    <Navbar>
+      <Container>
+        <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as: <a href="#login">Mark Otto</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default TextLinkExample;
