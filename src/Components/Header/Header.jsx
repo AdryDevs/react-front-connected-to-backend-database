cbafa2953e78ebab0180262789dbddc10ead93c1
Carlos <karucupcake@gmail.com>
2022-11-30 19:09:00 +0100

    user view started

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,45 +7,75 @@ import { Row, Col } from 'react-bootstrap';
 import './Header.scss'
 
 function Header() {
-  return (
-    <Row>
+  if(localStorage.getItem==""||localStorage.jwt==null){
+    return (
+      <Row>
+  
+        <Navbar className='Navbar' expand="sm">
+          <Container>
+            <Navbar.Brand href="/">Home</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Col>
+                  <Nav.Link href="/Login">Login</Nav.Link>
+                </Col>
+                <Col>
+                  <Nav.Link href="/Register">Register</Nav.Link>
+                </Col>
+                <Col>
+                  <Nav.Link href="/Movies">Movies</Nav.Link>
+                </Col>
+                <Col>
+                  <Nav.Link href="/Series">Series</Nav.Link>
+                </Col>
 
-      <Navbar className='Navbar' expand="sm">
-        <Container>
-          <Navbar.Brand href="/">Home</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Col>
-                <Nav.Link href="/Login">Login</Nav.Link>
-              </Col>
-              <Col>
-                <Nav.Link href="/Register">Register</Nav.Link>
-              </Col>
-              <Col>
-                <Nav.Link href="/Movies">Movies</Nav.Link>
-              </Col>
-              <Col>
-                <Nav.Link href="/Series">Series</Nav.Link>
-              </Col>
-              <NavDropdown className='UserSettings' title="User" id="basic-nav-dropdown">
-                <NavDropdown.Item href="Settings">Settings</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </Row>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Row>
+  
+    )
+  }else{
+    return (
+      <Row>
+  
+        <Navbar className='Navbar' expand="sm">
+          <Container>
+            <Navbar.Brand href="/">Home</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Col>
+                  <Nav.Link href="/Movies">Movies</Nav.Link>
+                </Col>
+                <Col>
+                  <Nav.Link href="/Series">Series</Nav.Link>
+                </Col>
+                <Col>
+                  <Nav.Link href="">Log Out</Nav.Link>
+                </Col>
+                <NavDropdown className='UserSettings' title="User" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="Settings">Settings</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Row>
+  
+    );
+  }
 
-  );
 }
 
 export default Header
