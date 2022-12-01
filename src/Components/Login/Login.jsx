4c3d295b03a4653f4a76c6bbc96499091550635e
Carlos <karucupcake@gmail.com>
2022-12-01 18:55:12 +0100

    checkout

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,36 +2,39 @@ import "./Login.scss"
 import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-const Login = () =>{
-  const [form, setForm] = useState({});
-  const setField = (field, value) => {
-    setForm({
-        ...form,
-        [field]: value
-    })
-}
+const Login = () => {
+    const navigate = useNavigate();
+    const [form, setForm] = useState({});
+    const setField = (field, value) => {
+        setForm({
+            ...form,
+            [field]: value
+        })
+    }
 
-const handleSubmit = (e) => {
-    let resp="";
-  e.preventDefault()
-  console.log(form);
-   axios.post("http://localhost:3002/auth/login",form)
-  .then(response=>{
-    console.log(response);
-    resp=response;
-    if(resp!==""){
-        localStorage.setItem('jwt', JSON.stringify(resp.data.jwt));
-      }
-      else{
-        console.log("No se ha guardado el token");
-      }
-  });
+    const handleSubmit = (e) => {
+        let resp = "";
+        e.preventDefault()
+        console.log(form);
+        axios.post("http://localhost:3002/auth/login", form)
+            .then(response => {
+                console.log(response);
+                resp = response;
+                if (resp !== "") {
+                    localStorage.setItem('jwt', JSON.stringify(resp.data.jwt));
+                    navigate("/");
+                }
+                else {
+                    console.log("No se ha guardado el token");
+                }
+            });
 
-}
+    }
 
     return (
-<Form>
+        <Form>
             <Form.Group controlId='email'>
                 <Form.Label>Username</Form.Label>
                 <Form.Control
@@ -59,6 +62,6 @@ const handleSubmit = (e) => {
                 </Button>
             </Form.Group>
         </Form>
-  );
+    );
 }
 export default Login;
