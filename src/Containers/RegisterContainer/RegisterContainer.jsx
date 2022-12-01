4de866b9c99fc1274a1251567fd4b925985c7cfa
Juan Genovard <juangenovardcaballero@gmail.com>
2022-12-01 18:28:37 +0100

    container editado

--- src/Containers/RegisterContainer/RegisterContainer.jsx
+++ src/Containers/RegisterContainer/RegisterContainer.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import Register from "../../Components/Register/Register";
+import './RegisterContainer.scss'
 const RegisterContainer = () => {
   return (
-    <Container fluid>
+    <Container fluid className='ContainerRegister'>
       <Row>
         <Col />
         <Col xs={10} sm={8} md={6} lg={5}>
