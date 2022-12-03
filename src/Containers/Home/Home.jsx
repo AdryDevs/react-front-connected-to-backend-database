47d2c6a8c4b4410fc12687f641c4b33d111b6706
Juan Genovard <juangenovardcaballero@gmail.com>
2022-12-03 13:28:20 +0100

    He quitado el carousel

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -3,65 +3,25 @@ import './Home.scss'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function UncontrolledExample() {
   return (
-    <Container fluid>
-    <Row className='Texto Fila'>
-      <Col>
-      <div >
-        Are your redy for the beast series in the world?<br />
-        Disfruta de un mundo lleno de<br />
-         posibilidades y 
-        aventuras en<br />
-         film FRESAS
-      </div>
-      </Col>
-      <Col className='ColCarousel'>
-    <Carousel className='Carousel1'>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://discordapp.com/channels/@me/1024729179918442557/1047917924171653160"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
-          alt="Second slide"
-        />
+    <Container className='Fondo' fluid>
+      <Row className='Texto Fila'>
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
-          alt="Third slide"
-        />
+        <Col> 
+          <div className='divtext' >
+            Are your redy for the beast series in the world?<br />
+            Disfruta de un mundo lleno de<br />
+            posibilidades y
+            aventuras en<br />
+            film FRESAS<br />
 
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-    </Col>
-    </Row>
+          </div>
+        </Col>
+      </Row>
     </Container>
   );
 }
